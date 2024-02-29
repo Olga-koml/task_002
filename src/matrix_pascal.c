3882afcecb42b0bf88a038a948c0dadf5b2c1f4e
Olga Komleva <olyakomleva@mail.ru>
2024-02-29 23:15:41 +0300

    fix clang format

--- src/matrix_pascal.c
+++ src/matrix_pascal.c
@@ -6,13 +6,13 @@
 int main() {
     int matrix_size;
     if (scanf("%d", &matrix_size) == 1 && matrix_size > 0 && matrix_size <= 10) {
-        int *matrix = (int *)malloc((matrix_size+matrix_size) * matrix_size * sizeof(int));
+        int *matrix = (int *)malloc((matrix_size + matrix_size) * matrix_size * sizeof(int));
         if (matrix == NULL) {
             fprintf(stderr, "Memory allocation error!");
             return EXIT_FAILURE;
         }
         int count = 0;
-        for (int i = 1; count <= matrix_size*matrix_size+1; ++i) {
+        for (int i = 1; count <= matrix_size*matrix_size + 1; ++i) {
             int base_matrix[i];
             find_base_triangle(base_matrix, i);
             for (int j = 0; j < i; ++j) {
@@ -25,7 +25,6 @@ int main() {
         fprintf(stderr, "Puck you, Verter!");
         return EXIT_FAILURE;
     }
-
     return 0;
 }
 
